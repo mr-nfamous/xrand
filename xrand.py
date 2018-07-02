fae38b9e1e2fc48a572ac8b4bd8f164c9cca9d17
Bup <mr.assume.away@gmail.com>
2018-07-02 00:20:40 -0500

    Create xrand.py

---
+++ xrand.py
@@ -0,0 +1,42 @@
+
+import _xrand
+
+# wanted this online asap...
+# this is temporary until everything is squished directly into the ext module
+
+rng = _xrand.Random()
+choices = rng.choices
+dice = rng.dice
+flip = rng.flip
+flips = rng.flips
+iter_beta = rng.iter_beta
+iter_expo = rng.iter_expo
+iter_gamma = rng.iter_gamma
+iter_log_normal = rng.iter_log_normal
+iter_normal = rng.iter_normal
+iter_pareto = rng.iter_pareto
+iter_triangular = rng.iter_triangular
+iter_uniform = rng.iter_uniform
+iter_vonmises = rng.iter_vonmises
+iter_weibull = rng.iter_weibull
+jump = rng.jump
+rand_bits = rng.rand_bits
+rand_bytes = rng.rand_bytes
+rand_float = rng.rand_float
+rand_index = rng.rand_index
+rand_int = rng.rand_int
+rv_beta = rng.rv_beta
+rv_expo = rng.rv_expo
+rv_gamma = rng.rv_gamma
+rv_log_normal = rng.rv_log_normal
+rv_normal = rng.rv_normal
+rv_pareto = rng.rv_pareto
+rv_triangular = rng.rv_triangular
+rv_uniform = rng.rv_uniform
+rv_vonmises = rng.rv_vonmises
+rv_weibull = rng.rv_weibull
+sample = rng.sample
+select = rng.select
+shuffle = rng.shuffle
+shufled = rng.shuffled
+split = rng.split
