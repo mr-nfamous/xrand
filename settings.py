5f05913103bd4c277f383820bae272ca59a9ca4a
Dan Snider <mr.assume.away@gmail.com>
2018-07-05 12:51:01 -0500

    added optional setstate function

--- settings.py
+++ settings.py
@@ -3,11 +3,11 @@
 # most of this probably doesn't work atm (if any does at all)
 
 
-Ext_MODULE = '_xrand'
+Ext_MODULE = 'xrand'
 Py_MODULE = 'brand' # test module to avoid name conflicts
 PROFILE_LOOPS = 25_000_000
 # debug mode: nothing works unless RAND_DEBUG is True
-RAND_DEBUG        = True
+RAND_DEBUG        = False
 RAND_TEST_FUNCS   = False
 DEBUG_PARSE_ARGS  = False
 
@@ -19,14 +19,15 @@ SAVE_AUTO_H       = False # builds auto.h
 # without FORCE_GENERATOR the generator chosen depends on platform
 XOROSHIRO         = 'xoroshiro128'
 XORSHIFT          = 'xorshift128'
-RNG_OVERRIDE      = XOROSHIRO # must be XOROSHIRO or XORSHIFT
+RNG_OVERRIDE      = None # must be XOROSHIRO or XORSHIFT
 
 # extras
-INCLUDE_NUMBERSET = False
-INCLUDE_PROFILER = True
+INCLUDE_NUMBERSET = False #include a lightly wrapped version of internal set
+INCLUDE_PROFILER = True #include "perf" function
 
+# is a module level function and should never actually be used
+INCLUDE_SETSTATE = False #enable setstate func
 
 COMPILE_ARGS = []
 MACROS = {'_MODULE_': Ext_MODULE,
           'PROFILE_LOOPS': str(PROFILE_LOOPS)}
-
